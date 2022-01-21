a96fc6180cabf6c60fdee7b7ccfb3772a4e6d17e
Felix Wallis <felixwallis944@gmail.com>
2022-01-21 15:07:57 +0000

    Text cleaning

--- geneiScraper.py
+++ geneiScraper.py
@@ -4,15 +4,9 @@ import json
 import re
 
 
-# with open('genei_notepad.html', 'r') as html_file:
-#     html = html_file.read()
-#     json_ = xmltojson.parse(html)
-# with open('data.json', 'w') as file:
-#     json.dump(json_, file)
-
 h1_text = []
 h2_text = []
-h3_text = [h1_text]
+h3_text = []
 
 
 def get_headers():
@@ -25,20 +19,45 @@ def get_headers():
         global all_text_string
         all_text_string = ''
         for text in all_text:
+            all_text_list = []
+            rl = re.split(r'\s', text)
+            for word in rl:
+                if len(word):
+                    all_text_list.append(word)
+            text = ' '.join(all_text_list)
             all_text_string += text
         for h1_element in h1_elements:
-            h1_text.append(h1_element.get_text())
+            h1_text_list = []
+            rl = re.split(r'\s', h1_element.get_text())
+            for word in rl:
+                if len(word):
+                    h1_text_list.append(word)
+            header = ' '.join(h1_text_list)
+            h1_text.append(header)
         for h2_element in h2_elements:
-            h2_text.append(h2_element.get_text())
+            h2_text_list = []
+            rl = re.split(r'\s', h2_element.get_text())
+            for word in rl:
+                if len(word):
+                    h2_text_list.append(word)
+            header = ' '.join(h2_text_list)
+            h2_text.append(header)
         for h3_element in h3_elements:
-            h3_text.append(h3_element.get_text())
+            h3_text_list = []
+            rl = re.split(r'\s', h3_element.get_text())
+            for word in rl:
+                if len(word):
+                    h3_text_list.append(word)
+            header = ' '.join(h3_text_list)
+            h3_text.append(header)
 
 
 def get_header_positions():
     for header in h1_text:
         rl = re.split(r'\s', header)
-        print(rl)
+        # print(rl)
 
 
 get_headers()
 get_header_positions()
+print(all_text_string)
