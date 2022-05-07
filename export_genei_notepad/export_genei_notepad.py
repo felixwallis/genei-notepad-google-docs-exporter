0203f65a2e8ff90717ec0417a3f11fb22aea6208
Felix Wallis <felixwallis944@gmail.com>
2022-05-07 01:14:05 +0100

    Rearranged functions

--- export_genei_notepad/export_genei_notepad.py
+++ export_genei_notepad/export_genei_notepad.py
@@ -6,6 +6,50 @@ import google_docs_formatting_helpers
 import google_docs_api_helpers
 
 
+def main():
+    print('Give this document a name: ')
+    title = input()
+
+    print('Scraping content...')
+
+    with open('../genei_notepad.html') as fp:
+        soup = BeautifulSoup(fp, 'html.parser')
+        get_all_text(soup.find_all(text=True))
+        h1_headers = create_header_arrays(soup.find_all('h1'))
+        h2_headers = create_header_arrays(soup.find_all('h2'))
+        h3_headers = create_header_arrays(soup.find_all('h3'))
+
+    # Check if h3 headers are present for Google Drive formatting logic
+    if len(h3_headers):
+        h3_present = True
+    else:
+        h3_present = False
+
+    print('Getting header positions...')
+    header_positions = []
+    for header_position in get_header_positions(h1_headers, 'h1', h3_present):
+        header_positions.append(header_position)
+    for header_position in get_header_positions(h2_headers, 'h2', h3_present):
+        header_positions.append(header_position)
+    for header_position in get_header_positions(h3_headers, 'h3', h3_present):
+        header_positions.append(header_position)
+
+    header_positions.sort(
+        key=lambda item: item['header_position'])
+
+    print('Generating document outline...')
+    document_outline = generate_document_outline(header_positions)
+
+    print('Processing document outline...')
+    processed_document_outline = process_document_outline(document_outline)
+    # Use this premade processed document outline when testing to prevent being
+    # charged for GPT-3 use
+    # processed_document_outline = premade_processed_document_outline()
+
+    print('Coverting document outline to Google Doc...')
+    covert_document_outline_to_google_doc(title, processed_document_outline)
+
+
 # Convert text in genei_notepad.html to string
 def get_all_text(all_text):
     # Store text string globally to be used by all the functions below
@@ -36,25 +80,6 @@ def create_header_arrays(header_elements):
     return header_text
 
 
-# Return index position of a specific header in all_text_string
-# Complexity is added by handling duplicate headers
-def get_header_position(header, header_type, h3_present, header_previous_occurance_position=None):
-    if header_previous_occurance_position == None:
-        header_index = all_text_string.index(header)
-    else:
-        text_to_search = all_text_string[header_previous_occurance_position + len(header):
-                                         len(all_text_string)]
-        header_index = text_to_search.index(
-            header) + (len(all_text_string) - len(text_to_search))
-
-    return {
-        'header': header,
-        'header_position': header_index,
-        'header_type': header_type,
-        'h3_present': h3_present
-    }
-
-
 # Return index positions of all headers of a given type in all_text_string
 # Complexity is added by handling duplicate headers
 def get_header_positions(headers, header_type, h3_present):
@@ -81,24 +106,23 @@ def get_header_positions(headers, header_type, h3_present):
     return type_specific_header_positions
 
 
-# Return text between two markers from all_text_string
-def retrieve_text_between_markers(marker_1, marker_2, header_length):
-    text_snippet = ''
-    for index, char in enumerate(all_text_string):
-        if marker_2 is not None:
-            if index >= (marker_1 + header_length) and index < marker_2:
-                text_snippet += char
-        else:
-            if index >= (marker_1 + header_length):
-                text_snippet += char
+# Return index position of a specific header in all_text_string
+# Complexity is added by handling duplicate headers
+def get_header_position(header, header_type, h3_present, header_previous_occurance_position=None):
+    if header_previous_occurance_position == None:
+        header_index = all_text_string.index(header)
+    else:
+        text_to_search = all_text_string[header_previous_occurance_position + len(header):
+                                         len(all_text_string)]
+        header_index = text_to_search.index(
+            header) + (len(all_text_string) - len(text_to_search))
 
-    if len(text_snippet):
-        text_snippet_obj = {
-            'text': text_snippet,
-            'text_type': 'unstyled'
-        }
-
-        return text_snippet_obj
+    return {
+        'header': header,
+        'header_position': header_index,
+        'header_type': header_type,
+        'h3_present': h3_present
+    }
 
 
 # Return genei_notepad.html outline based on headers, header positions, and text between headers
@@ -131,20 +155,49 @@ def generate_document_outline(header_positions):
     return document_outline
 
 
-# Return JSON object from generic GPT-3 request
-def make_openai_request(text_prompt, model):
-    response = openai.Completion.create(
-        engine=model,
-        prompt=text_prompt,
-        temperature=0.7,
-        max_tokens=1000,
-        top_p=1,
-        frequency_penalty=0.5,
-        presence_penalty=0.2
-    )
-    json_object = json.loads(str(response))
+# Return text between two markers from all_text_string
+def retrieve_text_between_markers(marker_1, marker_2, header_length):
+    text_snippet = ''
+    for index, char in enumerate(all_text_string):
+        if marker_2 is not None:
+            if index >= (marker_1 + header_length) and index < marker_2:
+                text_snippet += char
+        else:
+            if index >= (marker_1 + header_length):
+                text_snippet += char
 
-    return json_object
+    if len(text_snippet):
+        text_snippet_obj = {
+            'text': text_snippet,
+            'text_type': 'unstyled'
+        }
+
+        return text_snippet_obj
+
+
+# Return updated document_outline with text snippets processed using GPT-3
+def process_document_outline(document_outline):
+    with open('../openai_credentials.json', 'r') as fp:
+        json_data = json.load(fp)
+        openai.api_key = json_data['API_Key']
+
+    processed_document_outline = []
+    for index, text_element in enumerate(document_outline):
+        if text_element['text_type'] == 'unstyled':
+            # Check length of text to be processed does not exceed GPT-3 token limit
+            if (len(text_element['text'])/4) <= 1048:
+                text_element['processed_text'] = process_text_snippet_with_openai(
+                    text_element['text'])
+                processed_document_outline.append(text_element)
+            elif (len(text_element['text'])/4) >= 1048:
+                print('The following text snippet is too long for GPT-3 processing. Add a heading or subheading to split this snippet into smaller sections: ',
+                      '\n', text_element['text'])
+        else:
+            processed_document_outline.append(text_element)
+        print('Processed text element', (index + 1),
+              'of', len(document_outline))
+
+    return processed_document_outline
 
 
 # Return a text string turned into bullet points by GPT-3
@@ -172,29 +225,20 @@ def process_text_snippet_with_openai(text_to_process):
         return '. '.join(reprocessed_text)
 
 
-# Return updated document_outline with text snippets processed using GPT-3
-def process_document_outline(document_outline):
-    with open('../openai_credentials.json', 'r') as fp:
-        json_data = json.load(fp)
-        openai.api_key = json_data['API_Key']
+# Return JSON object from generic GPT-3 request
+def make_openai_request(text_prompt, model):
+    response = openai.Completion.create(
+        engine=model,
+        prompt=text_prompt,
+        temperature=0.7,
+        max_tokens=1000,
+        top_p=1,
+        frequency_penalty=0.5,
+        presence_penalty=0.2
+    )
+    json_object = json.loads(str(response))
 
-    processed_document_outline = []
-    for index, text_element in enumerate(document_outline):
-        if text_element['text_type'] == 'unstyled':
-            # Check length of text to be processed does not exceed GPT-3 token limit
-            if (len(text_element['text'])/4) <= 1048:
-                text_element['processed_text'] = process_text_snippet_with_openai(
-                    text_element['text'])
-                processed_document_outline.append(text_element)
-            elif (len(text_element['text'])/4) >= 1048:
-                print('The following text snippet is too long for GPT-3 processing. Add a heading or subheading to split this snippet into smaller sections: ',
-                      '\n', text_element['text'])
-        else:
-            processed_document_outline.append(text_element)
-        print('Processed text element', (index + 1),
-              'of', len(document_outline))
-
-    return processed_document_outline
+    return json_object
 
 
 # Example of a document outline processed by GPT-3
@@ -259,49 +303,5 @@ def covert_document_outline_to_google_doc(title, processed_document_outline):
         requests, document_id)
 
 
-def export_genei_notepad_to_google_doc():
-    print('Give this document a name: ')
-    title = input()
-
-    print('Scraping content...')
-
-    with open('../genei_notepad.html') as fp:
-        soup = BeautifulSoup(fp, 'html.parser')
-        get_all_text(soup.find_all(text=True))
-        h1_headers = create_header_arrays(soup.find_all('h1'))
-        h2_headers = create_header_arrays(soup.find_all('h2'))
-        h3_headers = create_header_arrays(soup.find_all('h3'))
-
-    # Check if h3 headers are present for Google Drive formatting logic
-    if len(h3_headers):
-        h3_present = True
-    else:
-        h3_present = False
-
-    print('Getting header positions...')
-    header_positions = []
-    for header_position in get_header_positions(h1_headers, 'h1', h3_present):
-        header_positions.append(header_position)
-    for header_position in get_header_positions(h2_headers, 'h2', h3_present):
-        header_positions.append(header_position)
-    for header_position in get_header_positions(h3_headers, 'h3', h3_present):
-        header_positions.append(header_position)
-
-    header_positions.sort(
-        key=lambda item: item['header_position'])
-
-    print('Generating document outline...')
-    document_outline = generate_document_outline(header_positions)
-
-    print('Processing document outline...')
-    processed_document_outline = process_document_outline(document_outline)
-    # Use this premade processed document outline when testing to prevent being
-    # charged for GPT-3 use
-    # processed_document_outline = premade_processed_document_outline()
-
-    print('Coverting document outline to Google Doc...')
-    covert_document_outline_to_google_doc(title, processed_document_outline)
-
-
 if __name__ == '__main__':
-    export_genei_notepad_to_google_doc()
+    main()
