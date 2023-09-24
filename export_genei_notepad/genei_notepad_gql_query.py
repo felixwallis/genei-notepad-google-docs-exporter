dd4a5cbea902615b6b7288f7194fd99007a64dff
Felix Wallis <felixwallis944@gmail.com>
2023-09-24 16:17:31 +0100

    first update for a while

--- export_genei_notepad/genei_notepad_gql_query.py
+++ export_genei_notepad/genei_notepad_gql_query.py
@@ -1,3 +1,4 @@
+import json
 import requests
 
 input = input('Enter the name of the note you want to export: ')
@@ -21,13 +22,16 @@ def search_genei_notes(input):
         }
     }'''
     url = 'https://dev.api.app.genei.io/graphql'
-    headers = {'Content-Type': 'application/json', 'x-api-key': 'sk_12f9accc6ff84c23a9cd1a67818fab0b'}
+    headers = {'Content-Type': 'application/json',
+               'x-api-key': 'sk_12f9accc6ff84c23a9cd1a67818fab0b'}
     r = requests.post(url, json=({'query': query}), headers=headers)
     return r.json()
+
 
 search_results = search_genei_notes(input)
 top_search_result = search_results['data']['searchByName']['results'][0]
 top_search_result_id = top_search_result['item']['id']
+
 
 def get_note_data(input):
     query = '''query {
@@ -36,14 +40,18 @@ def get_note_data(input):
         }
     }'''
     url = 'https://dev.api.app.genei.io/graphql'
-    headers = {'Content-Type': 'application/json', 'x-api-key': 'sk_12f9accc6ff84c23a9cd1a67818fab0b'}
+    headers = {'Content-Type': 'application/json',
+               'x-api-key': 'sk_12f9accc6ff84c23a9cd1a67818fab0b'}
     r = requests.post(url, json=({'query': query}), headers=headers)
     return r.json()
 
+
 note_data = get_note_data(top_search_result_id)['data']['note']['data']
+
 
 def process_note_data(note_data):
     content = note_data['root']
-    print(content)
+    json.dump(content, open('note.json', 'w'), indent=4)
+
 
 process_note_data(note_data)
